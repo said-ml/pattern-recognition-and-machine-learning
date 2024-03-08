a545d9d04fa8fd1a88afd0a146ab9f907509431b
said-ml <113719075+said-ml@users.noreply.github.com>
2024-03-09 03:59:36 +1000

    turn a directory into package

---
+++ Chapter9:Mixture And Models With EM/__init__.py
@@ -0,0 +1,7 @@
+class Nothing:
+    ''' turn this directory into a python package '''
+    pass
+
+from .distance import EuclideanDistance
+from .distance import ManhattanDistance
+from .distance import  CosineDistance
